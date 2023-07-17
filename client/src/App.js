35ce05aeb9b602aff4967b6f3bc20ca5608a7413
Donnie46 <122732624+Donnie46@users.noreply.github.com>
2023-07-16 22:27:48 -0500

    added dependencies for MUI and linked stylesheet in the index.html file

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {
   ApolloClient,
@@ -35,22 +34,8 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    
     </ApolloProvider>
   );
 }
